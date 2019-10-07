5bcd2e1595802bc54acb6a03ef33a68f1802f506
klok101 <blackhawks4447@gmail.com>
2019-10-07 11:59:06 -0500

    SCROOOOOOOOLLLLLLLLLL

---
+++ app/src/main/java/com/example/scrollbg/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.scrollbg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(100L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
+
+    }
+}
