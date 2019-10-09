68b050d2972ed206da3ccd6efe5b6e82afcb1723
indiamaowei7431 <ebzicorn001@gmail.com>
2019-10-09 11:13:17 -0500

    x scroller

--- app/src/main/java/com/example/scrollbg/MainActivity.java
+++ app/src/main/java/com/example/scrollbg/MainActivity.java
@@ -4,33 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int x = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_main);
 
+
+
         final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
         final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
 
-        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        final ValueAnimator animator = ValueAnimator.ofFloat(1.0f, 0.0f);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(2L);
+        animator.setDuration(10000L / x);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 final float progress = (float) animation.getAnimatedValue();
+                Log.i("big ", Float.toString(progress));
                 final float width = backgroundOne.getWidth();
                 final float translationX = width * progress;
                 backgroundOne.setTranslationX(translationX);
                 backgroundTwo.setTranslationX(translationX - width);
+
+                if (progress <= .01){
+                    x += 10;
+                    animator.setDuration(x);
+                }
+
+
             }
+
         });
+
         animator.start();
 
     }
